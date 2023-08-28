0189f25b500f88472d21b7a1004538acb63703de
AnastasiaKratschmer <111846502+AnastasiaKratschmer@users.noreply.github.com>
2023-08-28 16:37:58 +0200

    it's back to running, no more wrongly than normal. maybe try diff starting keys for traversal

--- only_new_algo_01/utils_copy.py
+++ only_new_algo_01/utils_copy.py
@@ -438,11 +438,10 @@ def my_traversal_simply(graph, starting_key):
         current_node = queue.pop(0)  # Get the first node from the queue
         
         if current_node in neighborhood:
-            if current_node != starting_key:
-                for successor in neighborhood[current_node]:
-                    if successor not in alignment_pairs: #just added to try to not handle weird first case!!
-                        alignment_pairs[successor] = current_node
-                        queue.append(successor)  # Add successor to the queue for further traversal
+            for successor in neighborhood[current_node]:
+                if successor not in alignment_pairs:
+                    alignment_pairs[successor] = current_node
+                    queue.append(successor)  # Add successor to the queue for further traversal
     if starting_key in alignment_pairs:
         del alignment_pairs[starting_key]
         
